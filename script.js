2530517e3227fc6d3918ede597bbdc0152a9b6d0
tejesh6138 <guditejesh68394@gmail.com>
2026-03-04 19:09:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// smooth scrolling animation
+
+const links = document.querySelectorAll("nav ul li a");
+
+links.forEach(link => {
+
+link.addEventListener("click", function(e){
+
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
